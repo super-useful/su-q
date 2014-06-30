d0b43a4b6166687611cab0ccf0c5484110a673fe
constantology <constantology@gmail.com>
2014-06-30 10:20:53 +0100

    added date coercion to mssql.Schema

--- test/helpers/requireall.test.js
+++ test/helpers/requireall.test.js
@@ -11,7 +11,7 @@ suite( 'suq.helpers.requireall', function() {
 	test( 'returns a the object of a the all a the modules in a the directory, recursively', function( done ) {
 		var modules = requireall( path.resolve( process.cwd(), 'lib', 'connection' ) );
 
-		expect( Object.keys( modules ) ).to.deep.equal( ['mongodb', 'test'] );
+		expect( Object.keys( modules ) ).to.deep.equal( ['mongodb', 'mssql', 'test'] );
 		expect( Object.keys( modules.mongodb ) ).to.deep.equal( ['collection', 'db', 'query'] );
 		expect( Object.keys( modules.test ) ).to.deep.equal( ['collection', 'db', 'query'] );
 
